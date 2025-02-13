9136fde18d33a59c386c323dd4972a7ecbe73770
Louis Laskar Ginting <louisginting54@gmail.com>
2025-02-13 13:49:56 +0700

    create splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:project_pkl/firebase_options.dart';
 import 'package:project_pkl/src/features/login/UI/login_screen.dart';
+import 'package:project_pkl/src/features/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginScreen(),
+      home: const SplashScreen(),
     );
   }
 }
--- lib/src/features/splash_screen.dart
+++ lib/src/features/splash_screen.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:project_pkl/src/features/login/UI/login_screen.dart';
+import 'package:project_pkl/src/tab_bar.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(Duration(seconds: 3), () {});
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => const LoginScreen()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset(
-        'images/splash_screen.png',
+      body: Center(
+        child: Image.asset(
+          'images/splash_screen.png',
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
       ),
     );
   }
